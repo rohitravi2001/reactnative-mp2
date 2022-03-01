85ddf434a2a6fb6aacb806c7fde2616ce54fb64e
Aniruth Narayanan <aniruth.n@berkeley.edu>
2022-02-28 18:00:17 -0800

    added comment explaining useUnicodeFlag plugin purpose

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
+    // fix for .useUnicodeFlag issue, see https://babeljs.io/docs/en/babel-plugin-proposal-unicode-property-regex
     plugins: [
       [
         "@babel/plugin-proposal-unicode-property-regex",
